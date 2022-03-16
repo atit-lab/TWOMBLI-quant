ad0be01195672b7100a4746f18e764eaefd8c34c
sakinkirti <sakin.arya.kirti@gmail.com>
2022-03-15 21:16:51 -0400

    fixed errors in the R script

--- PCA-analysis-03152022.R
+++ PCA-analysis-03152022.R
@@ -9,18 +9,17 @@ library(devtools)
 library(ggbiplot)
 
 # Importing the data
-filename <- "/Users/studentaccount/Documents/Sakin_exp/RGB Trichrome/RGB_all (2)/TWOMBLI_Results_img.csv"
-rgb <- read.csv(file=filename, header=TRUE, sep=",")
+filename <- "/Users/sakinkirti/Downloads/PCA 3-15-22.csv" # nolint
+rgb <- read.csv(file = filename, header = TRUE, sep = ",")
 
 # Covariance and multicolinearity
-rel_vars <- rgb[, c(2:8,19,20,22)]
-chart.Correlation(rel_vars, histogram=TRUE, method="pearson")
+rel_vars <- rgb[, c(2:8, 15, 16)]
+chart.Correlation(rel_vars, histogram = TRUE, method = "pearson")
 
 # PCA with all variables
-rgb.pca <- prcomp( rgb[ ,c(2:8,19,20,22) ], center=TRUE, scale.=TRUE )
-summary(rgb.pca)
+rgb_pca <- prcomp(rgb[, c(2:8, 15, 16)], center = TRUE, scale. = TRUE)
+summary(rgb_pca)
 
-ggbiplot(rgb.pca, ellipse=TRUE, groups=sort(rgb$Treatment), var.axes=FALSE, labels=rgb_resc$Image) + theme_bw()
+ggbiplot(rgb_pca, ellipse = TRUE, groups = sort(rgb$Treatment), var.axes = FALSE) + theme_bw() # nolint
 
 #---------- filtering features ----------#
-
